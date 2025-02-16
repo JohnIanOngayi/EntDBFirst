4baa4c4eb0aceb0ff5317f667d3d9e9ce84d3095
JohnIanOngayi <ongayijohnian@gmail.com>
2025-02-16 16:40:52 +0300

    Defined repository interface

--- srx/Contracts/IRepositoryBase.cs
+++ srx/Contracts/IRepositoryBase.cs
@@ -1,1 +1,12 @@
+using System.Linq.Expressions;
 
+namespace srx.Contracts;
+
+public interface IRepositoryBase<T>
+{
+    IQueryable<T> FindAll();
+    IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression);
+    void Create(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
